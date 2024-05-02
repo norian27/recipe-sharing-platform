cb7fad42460505f254682cbffdb7f5fa17a664d1
quarkusio <no-reply@quarkus.io>
2024-05-02 20:51:35 +0000

    Initial commit

---
+++ src/main/java/com/koh0118/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.koh0118;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/com/koh0118/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.koh0118;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
